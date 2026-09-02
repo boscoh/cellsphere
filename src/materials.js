557f80829cadfcb100dab6934243a7e249235cd7
boscoh <apposite@gmail.com>
2026-09-02 16:55:48 +0800

    size: halve tail bead radius to match smaller bacteria

--- src/materials.js
+++ src/materials.js
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 
 export const unitOuterGeo = new THREE.SphereGeometry(1, 40, 40)
 export const foodGeo = new THREE.SphereGeometry(0.05, 8, 8)
-export const tailGeo = new THREE.SphereGeometry(0.008, 6, 6)
+export const tailGeo = new THREE.SphereGeometry(0.004, 6, 6)
 
 export const foodMat = new THREE.MeshStandardMaterial({
   color: 0x9a917e,
